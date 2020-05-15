a92627fa4aaef468695bb53cf07dca43d4fa824d
Hidalgo, Nicolas <nicolashidalgocorrea@hotmail.com>
2020-05-15 00:12:35 -0500

    usuario bean

--- app/src/main/java/beans/UsuarioBean.java
+++ app/src/main/java/beans/UsuarioBean.java
@@ -4,10 +4,18 @@ import java.util.Date;
 
 public class UsuarioBean {
     private String ID;
+    private String ID_TIPO_DOCUMENTO;
+    private String NUM_DOCUMENTO;
+    private String NACIONALIDAD;
     private String NOMBRES;
     private String APELLIDOS;
     private String ID_EMPRESA;
     private String CORREO;
+    private String FECHA_NACIMIENTO;
+    private String NOMBRES_CONTACTO;
+    private String DIRECCION_CONTACTO;
+    private String TELEFONO_CONTACTO;
+    private String CORREO_CONTACTO;
     private String USUARIO;
     private String CONTRASENA;
     private String ID_ROL;
@@ -16,6 +24,70 @@ public class UsuarioBean {
     private String FEC_ELIMINACION;
 
     public UsuarioBean() {
+    }
+
+    public String getID_TIPO_DOCUMENTO() {
+        return ID_TIPO_DOCUMENTO;
+    }
+
+    public void setID_TIPO_DOCUMENTO(String ID_TIPO_DOCUMENTO) {
+        this.ID_TIPO_DOCUMENTO = ID_TIPO_DOCUMENTO;
+    }
+
+    public String getNUM_DOCUMENTO() {
+        return NUM_DOCUMENTO;
+    }
+
+    public void setNUM_DOCUMENTO(String NUM_DOCUMENTO) {
+        this.NUM_DOCUMENTO = NUM_DOCUMENTO;
+    }
+
+    public String getNACIONALIDAD() {
+        return NACIONALIDAD;
+    }
+
+    public void setNACIONALIDAD(String NACIONALIDAD) {
+        this.NACIONALIDAD = NACIONALIDAD;
+    }
+
+    public String getFECHA_NACIMIENTO() {
+        return FECHA_NACIMIENTO;
+    }
+
+    public void setFECHA_NACIMIENTO(String FECHA_NACIMIENTO) {
+        this.FECHA_NACIMIENTO = FECHA_NACIMIENTO;
+    }
+
+    public String getNOMBRES_CONTACTO() {
+        return NOMBRES_CONTACTO;
+    }
+
+    public void setNOMBRES_CONTACTO(String NOMBRES_CONTACTO) {
+        this.NOMBRES_CONTACTO = NOMBRES_CONTACTO;
+    }
+
+    public String getDIRECCION_CONTACTO() {
+        return DIRECCION_CONTACTO;
+    }
+
+    public void setDIRECCION_CONTACTO(String DIRECCION_CONTACTO) {
+        this.DIRECCION_CONTACTO = DIRECCION_CONTACTO;
+    }
+
+    public String getTELEFONO_CONTACTO() {
+        return TELEFONO_CONTACTO;
+    }
+
+    public void setTELEFONO_CONTACTO(String TELEFONO_CONTACTO) {
+        this.TELEFONO_CONTACTO = TELEFONO_CONTACTO;
+    }
+
+    public String getCORREO_CONTACTO() {
+        return CORREO_CONTACTO;
+    }
+
+    public void setCORREO_CONTACTO(String CORREO_CONTACTO) {
+        this.CORREO_CONTACTO = CORREO_CONTACTO;
     }
 
     public String getID_EMPRESA() {
