79a0b29d3384725ae73e57a549199665d2977910
Geduh <contact@hotmai.com>
2020-05-23 19:37:27 -0500

    diseño registro de fichas

--- app/src/main/java/com/example/covid19/EnfermedadesActivity.java
+++ app/src/main/java/com/example/covid19/EnfermedadesActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -13,6 +15,9 @@ import androidx.fragment.app.Fragment;
 
 public class EnfermedadesActivity extends Fragment {
     View v;
+    ListView listView;
+    ArrayAdapter<String> adapter;
+    String[] arrayEnfermedades = {"Hipertensión arterial","Asma","Enfermedad cardiovascular","Enfermedad respiratoria crónica","Cáncer","Insuficiencia renal","Diabetes","Tratamiento Inmunosupresor"};
     public EnfermedadesActivity(){
 
     }
@@ -21,6 +26,9 @@ public class EnfermedadesActivity extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         v = inflater.inflate(R.layout.activity_enfermedades,container, false);
+        listView = (ListView)v.findViewById(R.id.listViewEnfermedades);
+        adapter = new ArrayAdapter<String>(getActivity(),android.R.layout.simple_list_item_multiple_choice,arrayEnfermedades);
+        listView.setAdapter(adapter);
         return v;
     }
 }
