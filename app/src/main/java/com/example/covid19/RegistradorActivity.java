4ad1f8b98d748d0228712f5ff59d7a7beb1dc0c0
Geduh <contact@hotmai.com>
2020-06-03 01:27:12 -0500

    gv cambios empleado

--- app/src/main/java/com/example/covid19/RegistradorActivity.java
+++ app/src/main/java/com/example/covid19/RegistradorActivity.java
@@ -1,15 +1,28 @@
 package com.example.covid19;
 
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class RegistradorActivity extends AppCompatActivity {
 
+    FloatingActionButton btnAgregarRegistrador;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registrador);
+        btnAgregarRegistrador = findViewById(R.id.btnAgregarRegistrador);
+        btnAgregarRegistrador.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent dsp = new Intent(RegistradorActivity.this, ViewPageRegisterEmpleado.class);
+                startActivity(dsp);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/covid19/ViewPageRegisterEmpleado.java
@@ -0,0 +1,33 @@
+package com.example.covid19;
+
+import android.os.Bundle;
+
+import com.google.android.material.tabs.TabLayout;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager.widget.ViewPager;
+
+public class ViewPageRegisterEmpleado extends AppCompatActivity {
+
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+    private  ViewPagerAdapter viewPagerAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_page_register_empleado);
+        tabLayout = (TabLayout) findViewById(R.id.tabLayout_id);
+        viewPager = (ViewPager) findViewById(R.id.viewpager_id);
+        viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
+        final RegistroRegistradorActivity registroRegistradorActivity = new RegistroRegistradorActivity();
+        final AsignarSedeEmpleadoActivity asignarSedeEmpleadoActivity = new AsignarSedeEmpleadoActivity();
+        viewPagerAdapter.AddFragment(registroRegistradorActivity, "Empleado");
+        viewPagerAdapter.AddFragment(asignarSedeEmpleadoActivity,"Sedes");
+        viewPager.setOffscreenPageLimit(2);
+        viewPager.setAdapter(viewPagerAdapter);
+        tabLayout.setupWithViewPager(viewPager);
+        tabLayout.getTabAt(0).setIcon(R.drawable.ic_user_tap);
+        tabLayout.getTabAt(1).setIcon(R.drawable.ic_sede_tap);
+    }
+}
