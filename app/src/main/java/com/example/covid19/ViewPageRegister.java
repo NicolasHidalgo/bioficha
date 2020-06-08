ff951f705193a379bef233b77a967aee5bbfda48
Geduh <contact@hotmai.com>
2020-06-07 22:58:32 -0500

    gv cambios

--- app/src/main/java/com/example/covid19/ViewPageRegister.java
+++ app/src/main/java/com/example/covid19/ViewPageRegister.java
@@ -696,7 +696,6 @@ public class ViewPageRegister extends AppCompatActivity {
                         for (int i = 0; i < jsonArray.length(); i++) {
                             jsonObject = jsonArray.getJSONObject(i);
                             bean  = new SedePoligonoBean();
-                            bean.setID(jsonObject.getString("ID"));
                             bean.setLATITUD(jsonObject.getDouble("LATITUD"));
                             bean.setLONGITUD(jsonObject.getDouble("LONGITUD"));
                             bean.setID_SEDE(jsonObject.getString("ID_SEDE"));
--- app/src/main/java/db/DatabaseManagerSedePoligono.java
+++ app/src/main/java/db/DatabaseManagerSedePoligono.java
@@ -15,13 +15,11 @@ import beans.UsuarioSedeBean;
 public class DatabaseManagerSedePoligono extends DatabaseManager {
 
     public static final String NOMBRE_TABLA = "SEDE_POLIGONO";
-    public static final String CN_ID = "_ID";
     public static final String CN_LATITUD = "LATITUD";
     public static final String CN_LONGITUD = "LONGITUD";
     public static final String CN_ID_SEDE = "ID_SEDE";
 
     public static final String CREATE_TABLE =  "create table " + NOMBRE_TABLA + " ("
-            + CN_ID + " integer,"
             + CN_LATITUD + " double NULL,"
             + CN_LONGITUD + " double NULL,"
             + CN_ID_SEDE + " integer NULL"
@@ -37,7 +35,6 @@ public class DatabaseManagerSedePoligono extends DatabaseManager {
 
     private ContentValues generarContentValues(SedePoligonoBean obj){
         ContentValues valores = new ContentValues();
-        valores.put(CN_ID,obj.getID());
         valores.put(CN_LATITUD,obj.getLATITUD());
         valores.put(CN_LONGITUD,obj.getLONGITUD());
         valores.put(CN_ID_SEDE,obj.getID_SEDE());
@@ -50,13 +47,13 @@ public class DatabaseManagerSedePoligono extends DatabaseManager {
 
     public void actualizar(SedePoligonoBean obj) {
         ContentValues valores = generarContentValues(obj);
-        String [] args = new String[]{obj.getID()};
-        Log.d(NOMBRE_TABLA + "_actualiza",super.getDb().update(NOMBRE_TABLA,valores, CN_ID+ "=?", args)+"");
+        String [] args = new String[]{obj.getID_SEDE()};
+        Log.d(NOMBRE_TABLA + "_actualiza",super.getDb().update(NOMBRE_TABLA,valores, CN_ID_SEDE+ "=?", args)+"");
     }
 
     @Override
     public void eliminar(String id) {
-        super.getDb().delete(NOMBRE_TABLA,CN_ID+ "=?", new String[]{id});
+        super.getDb().delete(NOMBRE_TABLA,CN_ID_SEDE+ "=?", new String[]{id});
     }
 
     public void eliminarPorSede(String IdSede) {
@@ -72,21 +69,21 @@ public class DatabaseManagerSedePoligono extends DatabaseManager {
     @Override
     public Cursor cargar() {
         String [] columnas = new String[]
-                {CN_ID,CN_LATITUD,CN_LONGITUD,CN_ID_SEDE};
+                {CN_LATITUD,CN_LONGITUD,CN_ID_SEDE};
         return super.getDb().query(NOMBRE_TABLA, columnas,null,null,null,null,null);
     }
 
     @Override
     public Cursor cargarById(String id) {
         String [] columnas = new String[]
-                {CN_ID,CN_LATITUD,CN_LONGITUD,CN_ID_SEDE};
-        return super.getDb().query(NOMBRE_TABLA, columnas,CN_ID + "=?", new String[]{id},null,null,null);
+                {CN_LATITUD,CN_LONGITUD,CN_ID_SEDE};
+        return super.getDb().query(NOMBRE_TABLA, columnas,CN_ID_SEDE + "=?", new String[]{id},null,null,null);
     }
 
 
     public Cursor cargarPorTipo(String tipo) {
         String [] columnas = new String[]
-                {CN_ID,CN_LATITUD,CN_LONGITUD,CN_ID_SEDE};
+                {CN_LATITUD,CN_LONGITUD,CN_ID_SEDE};
         return super.getDb().query(NOMBRE_TABLA, columnas,CN_ID_SEDE+ " = ?",new String[] { tipo },null,null,null);
     }
 
@@ -94,7 +91,7 @@ public class DatabaseManagerSedePoligono extends DatabaseManager {
     @Override
     public Boolean compruebaRegistro(String id) {
         boolean existe = true;
-        Cursor resultSet = super.getDb().rawQuery("Select * from " + NOMBRE_TABLA + " WHERE " + CN_ID + " =" + id, null);
+        Cursor resultSet = super.getDb().rawQuery("Select * from " + NOMBRE_TABLA + " WHERE " + CN_ID_SEDE + " =" + id, null);
 
         if (resultSet.getCount() <= 0)
             existe = false;
@@ -129,10 +126,9 @@ public class DatabaseManagerSedePoligono extends DatabaseManager {
         SedePoligonoBean bean = null;
         while (c.moveToNext()){
             bean = new SedePoligonoBean();
-            bean.setID(c.getString(0));
-            bean.setLATITUD(c.getDouble(1));
-            bean.setLONGITUD(c.getDouble(2));
-            bean.setID_SEDE(c.getString(3));
+            bean.setLATITUD(c.getDouble(0));
+            bean.setLONGITUD(c.getDouble(1));
+            bean.setID_SEDE(c.getString(2));
             list.add(bean);
         }
         return list;
@@ -145,10 +141,10 @@ public class DatabaseManagerSedePoligono extends DatabaseManager {
 
         while (c.moveToNext()){
             bean = new SedePoligonoBean();
-            bean.setID(c.getString(0));
-            bean.setLATITUD(c.getDouble(1));
-            bean.setLONGITUD(c.getDouble(2));
-            bean.setID_SEDE(c.getString(3));
+            bean.setLATITUD(c.getDouble(0));
+            bean.setLONGITUD(c.getDouble(1));
+            bean.setID_SEDE(c.getString(2));
+
         }
         return bean;
     }
